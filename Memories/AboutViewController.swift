be7460ec2410bb9dda280271939249188cca1a88
Bepen Neupane <bepen3@gmail.com>
2018-11-17 16:39:54 -0500

    about view done

--- Memories/AboutViewController.swift
+++ Memories/AboutViewController.swift
@@ -16,6 +16,7 @@ class AboutViewController: UIViewController {
     @IBOutlet var appLogo: UIImageView!
     @IBOutlet var launchLabel: UILabel!
     @IBOutlet var accessLabel: UILabel!
+    @IBOutlet var button: UIButton!
     @IBOutlet var copyrightLabel: UILabel!
     
     let defaults = UserDefaults(suiteName: kAppGroupBundleID)!
@@ -24,6 +25,7 @@ class AboutViewController: UIViewController {
         super.viewDidLoad()
         
         //initializes the about view with the proper bundle values
+        button.setTitle(NSLocalizedString("str_done", comment: ""), for: .normal)
         nameLabel.text = Bundle.main.displayName
         versionLabel.text = Bundle.main.version
         buildLabel.text = Bundle.main.build
@@ -36,6 +38,11 @@ class AboutViewController: UIViewController {
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func onButton(_ sender: Any) {
+        //dismisses the about view controler
+        presentingViewController?.dismiss(animated: true, completion: nil)
     }
     
 }
